c453addeac4998a659fb207e5c6d676151b4ca7a
MartinSchwingenschuh <schwingenschuh.martin@gmail.com>
2023-05-15 11:20:25 +0200

    init

---
+++ server/src/main/java/com/protose/App.java
@@ -0,0 +1,17 @@
+package com.protose;
+
+
+import com.protose.shared.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello from server" );
+        Shared testObj = new Shared(10);
+    }
+}
---
+++ shared/Shared.java
@@ -0,0 +1,15 @@
+package com.protose.shared;
+
+/**
+ * Shared
+ */
+public class Shared {
+
+    int local;
+    String name;
+
+    public Shared(int i){
+        this.local = i;
+        this.name = "blub";
+    }   
+}
